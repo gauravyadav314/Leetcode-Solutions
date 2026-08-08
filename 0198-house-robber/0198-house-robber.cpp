1e33f01fed26501aba49e1e1268ad42a031cb703
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-08 09:52:43 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int ind, vector<int>& dp) {
-        if(ind >= nums.size()) {
-            return 0;
-        }
-        if(dp[ind] != -1) return dp[ind];
-        int take = helper(nums, ind+2, dp) + nums[ind];
-        int notTake = helper(nums, ind+1, dp);
-        return dp[ind] = max(take, notTake);
-    }
+// MEMOIZATION
+    // int helper(vector<int>& nums, int ind, vector<int>& dp) {
+    //     if(ind >= nums.size()) {
+    //         return 0;
+    //     }
+    //     if(dp[ind] != -1) return dp[ind];
+    //     int take = helper(nums, ind+2, dp) + nums[ind];
+    //     int notTake = helper(nums, ind+1, dp);
+    //     return dp[ind] = max(take, notTake);
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     // int sum = 0;
+    //     int n = nums.size();
+    //     vector<int> dp(n+1, -1);
+    //     return helper(nums, 0, dp);
+    // }
+
+// TABULATION 
     int rob(vector<int>& nums) {
-        int sum = 0;
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-        return helper(nums, 0, dp);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+        for(int i=2; i<n; i++) {
+            int take = nums[i] + dp[i-2];
+            int notTake = dp[i-1];
+            dp[i] = max(take, notTake);
+        }
+        return dp[n-1];
     }
+
 };
