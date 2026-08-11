87efe17cbce342afec7db9c6732a18f864949134
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-12 00:54:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (77.84%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -17,20 +17,39 @@ public:
     // }
 
     // tabulation 
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int>(n, 0));
         
+    //     for(int i=0; i<m; i++) {
+    //         for(int j=0; j<n; j++) {
+    //             if(i==0 && j==0) dp[0][0] = 1;
+    //             else {
+    //                 int down=0, right=0;
+    //                 if(i > 0) down = dp[i-1][j];
+    //                 if(j > 0) right = dp[i][j-1];
+    //                 dp[i][j] = down+right;
+    //             }
+    //         }
+    //     }
+    //     return dp[m-1][n-1];
+    // }
+
+    // space optimization
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 0);
         for(int i=0; i<m; i++) {
+            vector<int> temp(n, 0);
             for(int j=0; j<n; j++) {
-                if(i==0 && j==0) dp[0][0] = 1;
+                if(i==0 && j==0) temp[0] = 1;
                 else {
                     int down=0, right=0;
-                    if(i > 0) down = dp[i-1][j];
-                    if(j > 0) right = dp[i][j-1];
-                    dp[i][j] = down+right;
+                    if(i > 0) down = dp[j];
+                    if(j>0) right = temp[j-1];
+                    temp[j] = down + right;
                 }
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
