da7c41ada367a4d30c240d679890a86e0695d8c3
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-12 23:06:04 +0530

    Time: 0 ms (100%), Space: 16.4 MB (42.95%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& dp) {
-        if(r < 0 || c < 0) return INT_MAX;
-        if(r==0 && c==0) {
-            return grid[r][c];
-        }
-        if(dp[r][c] != -1) return dp[r][c];
-        int left = helper(grid, r, c-1, dp);
-        int up = helper(grid, r-1, c, dp);
-        return dp[r][c] = grid[r][c] + min(left, up);
-    }
+    // memoization 
+    // int helper(vector<vector<int>>& grid, int r, int c, vector<vector<int>>& dp) {
+    //     if(r < 0 || c < 0) return INT_MAX;
+    //     if(r==0 && c==0) {
+    //         return grid[r][c];
+    //     }
+    //     if(dp[r][c] != -1) return dp[r][c];
+    //     int left = helper(grid, r, c-1, dp);
+    //     int up = helper(grid, r-1, c, dp);
+    //     return dp[r][c] = grid[r][c] + min(left, up);
+    // }
+    // int minPathSum(vector<vector<int>>& grid) {
+    //     int row = grid.size();
+    //     int col = grid[0].size();
+    //     vector<vector<int>> dp(row, vector<int>(col, -1));
+    //     return helper(grid, row-1, col-1, dp);
+    // }
+
+    // tabulation 
     int minPathSum(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        vector<vector<int>> dp(row, vector<int>(col, -1));
-        return helper(grid, row-1, col-1, dp);
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                else {
+                    int left=INT_MAX, up=INT_MAX;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = grid[i][j] + min(left, up);
+                }
+            }
+        }
+        return dp[row-1][col-1];
     }
 };
