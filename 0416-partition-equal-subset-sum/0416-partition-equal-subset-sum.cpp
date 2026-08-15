78ebfbbdc9a7b309eec149b2e54f19ba023ddd56
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-15 10:16:41 +0530

    Time: 139 ms (45.57%), Space: 16.8 MB (72.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,6 +26,33 @@ public:
     // }
 
     // Tabular
+    // bool canPartition(vector<int>& nums) {
+    //     int sum = 0;
+    //     int n = nums.size();
+    //     for(int x : nums) {
+    //         sum += x;
+    //     }
+    //     if(sum%2 != 0) return false;
+    //     sum = sum/2;
+    //     vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
+    //     for(int i=0; i<n; i++) dp[i][0] = true;
+    //     if (nums[0] <= sum) {
+    //         dp[0][nums[0]] = true;
+    //     }
+    //     for(int ind=1; ind<n; ind++) {
+    //         for(int target=1; target<=sum; target++) {
+    //             bool notTaken = dp[ind-1][target];
+    //             bool taken = false;
+    //             if(nums[ind] <= target) {
+    //                 taken = dp[ind-1][target-nums[ind]];
+    //             }
+    //             dp[ind][target] = taken||notTaken;
+    //         }
+    //     }
+    //     return dp[n-1][sum];
+    // }
+
+    // SpaceOptimization
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
@@ -34,21 +61,21 @@ public:
         }
         if(sum%2 != 0) return false;
         sum = sum/2;
-        vector<vector<bool>> dp(n, vector<bool>(sum+1, false));
-        for(int i=0; i<n; i++) dp[i][0] = true;
-        if (nums[0] <= sum) {
-            dp[0][nums[0]] = true;
-        }
+        vector<bool> prev(sum+1, false);
+        prev[0] = true; 
+        if(nums[0] <= sum) prev[nums[0]] = true;
         for(int ind=1; ind<n; ind++) {
+            vector<bool> curr(sum+1, false);
             for(int target=1; target<=sum; target++) {
-                bool notTaken = dp[ind-1][target];
+                bool notTaken = prev[target];
                 bool taken = false;
                 if(nums[ind] <= target) {
-                    taken = dp[ind-1][target-nums[ind]];
+                    taken = prev[target-nums[ind]];
                 }
-                dp[ind][target] = taken||notTaken;
+                curr[target] = taken || notTaken;
             }
+            prev = curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
