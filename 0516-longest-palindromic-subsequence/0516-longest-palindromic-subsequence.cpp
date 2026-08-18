cd8d4c90cbaa4b8b365162ce41ea2921f0ca5840
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-18 21:39:08 +0530

    Time: 94 ms (23.26%), Space: 76.1 MB (22.97%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
-    int helper(string& s, string& t, int ind1, int ind2, vector<vector<int>>& dp) {
-        if(ind1 < 0 || ind2 < 0) return 0;
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        if(s[ind1] == t[ind2]) {
-            return dp[ind1][ind2] = (1 + helper(s, t, ind1-1, ind2-1, dp));
-        }
-        return dp[ind1][ind2] = max(helper(s, t, ind1-1, ind2, dp), helper(s, t, ind1, ind2-1, dp));
-    }
+// memoization
+    // int helper(string& s, string& t, int ind1, int ind2, vector<vector<int>>& dp) {
+    //     if(ind1 < 0 || ind2 < 0) return 0;
+    //     if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+    //     if(s[ind1] == t[ind2]) {
+    //         return dp[ind1][ind2] = (1 + helper(s, t, ind1-1, ind2-1, dp));
+    //     }
+    //     return dp[ind1][ind2] = max(helper(s, t, ind1-1, ind2, dp), helper(s, t, ind1, ind2-1, dp));
+    // }
+    // int longestPalindromeSubseq(string s) {
+    //     string t = s;
+    //     reverse(t.begin(), t.end());
+    //     int n = s.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+    //     return helper(s, t, n-1, n-1, dp);
+    // }
+
+// tabulation
     int longestPalindromeSubseq(string s) {
         string t = s;
         reverse(t.begin(), t.end());
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return helper(s, t, n-1, n-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                if(s[i-1] == t[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
     }
 };
