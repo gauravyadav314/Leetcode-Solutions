13764b19f09e5e1cec96131404d303a541b46c37
Gaurav Yadav <gauravji2005@gmail.com>
2026-08-24 18:44:29 +0530

    Time: 0 ms (100%), Space: 22.6 MB (11.55%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int i=0;
+        int j=n-1;
+        int ans=0;
+        while(i<=j) {
+            int mid = i+(j-i)/2;
+            if(citations[mid] >= (n-mid)) {
+                ans = n-mid;
+                j = mid-1;
+            }
+            else i = mid+1;
+        }
+        return ans;
+    }
+};
