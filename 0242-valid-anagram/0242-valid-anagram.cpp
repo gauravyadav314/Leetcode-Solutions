3e4be48a684fe8d60ed161994f43985760c5ec6f
Gaurav Yadav <gauravji2005@gmail.com>
2026-09-02 19:48:39 +0530

    Time: 2 ms (49.83%), Space: 9.7 MB (49.17%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
+        if(n != t.size()) return false;
+        unordered_map<char, int> mp;
+        for(auto& ch : s) mp[ch]++;
+        for(auto& ch : t) {
+            if(mp.find(ch) != mp.end()) {
+                mp[ch]--;
+                if(mp[ch]==0) mp.erase(ch);
+            }
+            else return false;
+        }
+        if(mp.empty()) return true;
+        return false;
+    }
+};
