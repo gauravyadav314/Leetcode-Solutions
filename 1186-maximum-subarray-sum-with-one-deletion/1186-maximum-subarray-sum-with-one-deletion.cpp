18c6253cdcc081161fd99bc1a70d263cf026eb62
Gaurav Yadav <gauravji2005@gmail.com>
2026-09-02 13:10:13 +0530

    Time: 36 ms (5.03%), Space: 39.6 MB (5.52%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
+    int helper(vector<int>& arr, int i, int deleted, vector<vector<int>>& dp) {
+        if(i<0) return -1e9;
+        if(dp[i][deleted] != -1) return dp[i][deleted];
+        if(deleted == 0) {
+            int newSt = arr[i];
+            int extend = arr[i] + helper(arr, i-1, deleted, dp);
+            int del = helper(arr, i-1, 1, dp);
+            return dp[i][deleted] = max({newSt, extend, del});
+        }
+        else {
+            int newSt = arr[i];
+            int extend = arr[i] + helper(arr, i-1, deleted, dp);
+            return dp[i][deleted] = max(newSt, extend);
+        }
+    }
+
     int maximumSum(vector<int>& arr) {
+        int ans = -1e9;
 
-        int noDelete = arr[0];
-        int oneDelete = INT_MIN / 2;
+        int n = arr.size();
 
-        int ans = arr[0];
+        vector<vector<int>> dp(n, vector<int>(2, -1));
 
-        for(int i = 1; i < arr.size(); i++) {
-
-            int oldNoDelete = noDelete;
-
-            noDelete = max(arr[i],
-                           noDelete + arr[i]);
-
-            oneDelete = max(oldNoDelete,
-                            oneDelete + arr[i]);
-
-            ans = max(ans, max(noDelete, oneDelete));
+        for (int i = 0; i < arr.size(); i++) {
+            ans = max(ans, helper(arr, i, 0, dp));
+            ans = max(ans, helper(arr, i, 1, dp));
         }
 
         return ans;
