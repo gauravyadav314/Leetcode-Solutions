0ad1bc5f4e74dae90f00126cc7d9280cd2076c82
Gaurav Yadav <gauravji2005@gmail.com>
2026-09-07 16:37:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     const int mod = 1000000007;
-    int helper(string& s, int ind, int lastInd, vector<vector<int>>& dp) {
-        if(ind == s.size()) {
-            if(lastInd > -1) return 1;
-            else return 0;
-        }
-        if(dp[ind+1][lastInd+1] != -1) return dp[ind+1][lastInd+1];
-        int notTake=0;
-        int take = helper(s, ind+1, ind, dp);
-        if(lastInd == -1) notTake = helper(s, ind+1, lastInd, dp);
-        else if(s[lastInd] != s[ind]) notTake = helper(s, ind+1, lastInd, dp);
-        return dp[ind+1][lastInd+1] = (take + notTake)%mod;
-    }
+// dp memoization
+    // int helper(string& s, int ind, int lastInd, vector<vector<int>>& dp) {
+    //     if(ind == s.size()) {
+    //         if(lastInd > -1) return 1;
+    //         else return 0;
+    //     }
+    //     if(dp[ind+1][lastInd+1] != -1) return dp[ind+1][lastInd+1];
+    //     int notTake=0;
+    //     int take = helper(s, ind+1, ind, dp);
+    //     if(lastInd == -1) notTake = helper(s, ind+1, lastInd, dp);
+    //     else if(s[lastInd] != s[ind]) notTake = helper(s, ind+1, lastInd, dp);
+    //     return dp[ind+1][lastInd+1] = (take + notTake)%mod;
+    // }
+    // int distinctSubseqII(string s) {
+    //     int n = s.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+    //     return helper(s, 0, -1, dp);
+    // }
+
+// dp space optimization
     int distinctSubseqII(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return helper(s, 0, -1, dp);
+        long long dp = 0;
+        vector<long long> hash(26, 0);
+        for(char ch : s) {
+            long long last = dp;
+            dp = ((2* dp) + 1 - hash[ch-'a'] + mod)%mod;
+            hash[ch-'a'] = last + 1;
+        }
+        return dp;
     }
 };
