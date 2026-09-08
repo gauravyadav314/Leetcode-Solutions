fb108efad7eed668aeb5e58fff1d2dbe913d0614
Gaurav Yadav <gauravji2005@gmail.com>
2026-09-08 11:13:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string ans = to_string(n);
-        int size = ans.size();
-        if(size<=3) return 0;
+        if(n < 1000) return 0;
         return n-1000+1;
     }
 };
